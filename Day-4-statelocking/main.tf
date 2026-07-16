f92da68603af107e100613b670dc1db3e6c8fcfd
maheshkolapalli1998-blip <maheshkolapalli1998@gmail.com>
2026-07-16 16:00:02 +0100

    deleted vpc

--- Day-4-statelocking/main.tf
+++ Day-4-statelocking/main.tf
@@ -5,12 +5,3 @@ resource "aws_instance" "name" {
         Name = "Hello"
     }
 }
-
-
-
-resource "aws_vpc" "my_vpc" {
-    cidr_block = "10.0.0.0/16"
-    tags = {
-        Name = "my_vpc"
-    }
-}
