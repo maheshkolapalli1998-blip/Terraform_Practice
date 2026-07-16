f1d2d04e4fe9d31ad808ded41a1d628d523f3f5c
maheshkolapalli1998-blip <maheshkolapalli1998@gmail.com>
2026-07-16 16:30:39 +0100

    changeses

--- Day-4-statelocking/backend.tf
+++ Day-4-statelocking/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket = "mysecondbucketesssess"
     key    = "path/to/my/key"
     region = "us-east-1"
-    use_lockfile = true #supports state locking only below versions of terraform 0.9.0 and above
+    #use_lockfile = true #supports state locking only below versions of terraform 0.9.0 and above
     dynamodb_table = "my-second-lock-table" #supports state locking only below versions of terraform 0.9.0 and above}
 }
 }
